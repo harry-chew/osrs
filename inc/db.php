bce45ba45b599947bc9429edcc8b1b772fc94374
Harry Chew <60672261+harry-chew@users.noreply.github.com>
2022-05-11 04:28:29 +0100

    Update db.php

--- inc/db.php
+++ inc/db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "dbuser";
-$password = "dbpass";
-$dbname = "osrs";
+$servername = "****";
+$username = "****";
+$password = "****";
+$dbname = "****";
 
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
